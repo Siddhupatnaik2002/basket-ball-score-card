280977137451867b3dfc3b3acb50cbca3eda235f
Venkumahathi Bhanu Shankara Siddhardha <97669766+Siddhupatnaik2002@users.noreply.github.com>
2026-04-16 00:56:57 +0530

    Add files via upload

---
+++ BasketBall_score_card/index.js
@@ -0,0 +1,75 @@
+
+let homePoint = 0;
+let guestPoint = 0;
+
+let homeScore = document.getElementById("home-el");
+
+let guestScore = document.getElementById("guest-el");
+
+function add_1(team){
+    if (team == "home"){
+        homePoint+=1;
+        homeScore.textContent = homePoint;
+    }
+    else{
+        guestPoint+=1;
+        guestScore.textContent = guestPoint;
+    }
+
+    highScore();
+}
+
+function add_2(team){
+    if (team == "home"){
+        homePoint+=2;
+        homeScore.textContent = homePoint;
+    }
+    else{
+        guestPoint+=2;
+        guestScore.textContent = guestPoint;
+    }
+    highScore();
+}
+
+function add_3(team){
+    if (team == "home"){
+        homePoint+=3;
+        homeScore.textContent = homePoint;
+    }
+    else{
+        guestPoint+=3;
+        guestScore.textContent = guestPoint;
+    }
+    highScore();
+}
+
+function newGame(){
+    homePoint = 0;
+    homeScore.textContent=0;
+
+    guestPoint = 0;
+    guestScore.textContent = 0;
+
+    homeScore.style.textShadow = " 0 0 0 ";
+    homeScore.style.backgroundColor = "#080001"
+
+    guestScore.style.textShadow = " 0 0 0 ";
+    guestScore.style.backgroundColor = "#080001";
+}
+
+function highScore(){
+
+    if (homePoint>guestPoint){
+        guestScore.style.textShadow = " 0 0 0 ";
+        guestScore.style.backgroundColor = "#080001";
+        homeScore.style.textShadow = " 0 0 3px black";
+        homeScore.style.backgroundColor = "#E3B505"
+    }
+
+    else if (homePoint<guestPoint){
+        homeScore.style.textShadow = " 0 0 0 ";
+        homeScore.style.backgroundColor = "#080001"
+        guestScore.style.textShadow = " 0 0 3px black";
+        guestScore.style.backgroundColor = "#E3B505";
+    }
+}
